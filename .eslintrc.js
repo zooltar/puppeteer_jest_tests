93f5b876a92ced5ddc4cfde28975e1d706f838b3
Przemyslaw Mazur <zoolt4n@Przemyslaws-MacBook-Pro.local>
2022-07-09 09:52:37 +0200

    Add & configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+    root: true,
+    extends: ['prettier'],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    env: {
+      node: true,
+      jest: true,
+      browser: true,
+    },
+    globals: {
+      page: true,
+      browser: true,
+      context: true,
+      jestPuppeteer: true,
+    },
+    rules: {
+      'class-methods-use-this': 'off',
+      'no-shadow': 'off',
+      'no-param-reassign': 'off',
+      'no-use-before-define': 'off',
+      'import/prefer-default-export': 'off',
+      'no-restricted-globals': [
+        'error',
+        {
+          name: 'fit',
+          message: 'Do not commit focused tests.',
+        },
+        {
+          name: 'fdescribe',
+          message: 'Do not commit focused tests.',
+        },
+      ],
+    },
+  }
